a26589e30d2b4c20c2b61b530f3e33159f6cae4c
Hajara Mohamed Rifky <124401488+HajaraRifky@users.noreply.github.com>
2024-01-18 21:12:10 +0000

    Update script.js

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -9,6 +9,7 @@ function showText(num, textid) {
 
     const textToChange = document.getElementById(textid);
     textToChange.className = "texttoshow";
+    console.log(getImagePath[num].src == 'assets/images/icon-plus.svg')
 
     if (getImagePath[num].src == 'assets/images/icon-plus.svg') {
         getImagePath[num].src = "assets/images/icon-minus.svg"
