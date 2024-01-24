7a83ece62747a0e1502f8ed23ba4bd66e1c9d697
Hajara Rifky <hajararifky@gmail.com>
2024-01-24 19:35:49 +0000

    Brushing up the code

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,19 +1,18 @@
 var getImagePath = document.getElementsByClassName("showbtn")
-var gettext = document.getElementsByClassName("texttoshow")
 var pTagContents = document.getElementsByTagName("p")
 
-function showText(num, textid) {
+function showText(sectionNum, textid) {
 
     changeStatus()//function to close ever text other than the one selected
 
     const textToChange = document.getElementById(textid);
     textToChange.className = "texttoshow";
-    console.log(getImagePath[num].src)
+    console.log(getImagePath[sectionNum].src)
 
-    if (getImagePath[num].src === "https://hajararifky.github.io/FAQ-Page-Challenge-Building-with-only-a-sketch/assets/images/icon-plus.svg"){
-        getImagePath[num].src = "assets/images/icon-minus.svg"
+    if (getImagePath[sectionNum].src === "https://hajararifky.github.io/FAQ-Page-Challenge-Building-with-only-a-sketch/assets/images/icon-plus.svg"){
+        getImagePath[sectionNum].src = "assets/images/icon-minus.svg"
         textToChange.className = "textfromhide";
-        pTagContents[num].className = "textfromhide";
+        pTagContents[sectionNum].className = "textfromhide";
 
     }
 
@@ -24,7 +23,6 @@ function changeStatus() {
     for (let index = 0; index < pTagContents.length; index++) {
 
         if (pTagContents[index].className != "inactive") {
-
             getImagePath[index].src = "assets/images/icon-plus.svg"
             pTagContents[index].className = "texttoshow";
 
